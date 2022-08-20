9ce05279e95ad8122d0ce24853689f12bbb8d8d9
Gabriel RICARD <44494841+SlyverStorm@users.noreply.github.com>
2022-08-20 17:36:04 +0200

    Update WeatherForecastController.cs

--- devops-backend/Controllers/WeatherForecastController.cs
+++ devops-backend/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace devops_backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Slmccl"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -21,11 +21,12 @@ namespace devops_backend.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, ).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Test = "test changement"
             })
             .ToArray();
         }
